49806864e04e5e2455bc3193cdc184953b3a7289
Brian Dixon <bjdixon@gmail.com>
2018-10-05 14:08:02 +0800

    add test coverage

--- truffle-config.js
+++ truffle-config.js
@@ -15,6 +15,18 @@ require('babel-polyfill');
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*',
+        },
+        coverage: {
+            host: 'localhost',
+            network_id: '*',
+            port: 8555,
+            gas: 0xfffffffffff,
+            gasPrice: 0x01,
+        }
+    }
 };
